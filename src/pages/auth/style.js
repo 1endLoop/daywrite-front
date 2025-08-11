8c8ffd7be3fb2e3102799ee40a2a7f0ce6386e91
yuna0l <kimyuna03140@gmail.com>
2025-08-11 19:22:37 +0900

    로그인페이지, 카테고리 페이지 배경설정 완료

--- src/pages/auth/style.js
+++ src/pages/auth/style.js
@@ -5,23 +5,23 @@ const S = {};
 // 전체 뷰 고정
 S.Container = styled.div`
   width: 100%;
-  height: 100vh;
-  /* overflow: hidden; */
+  min-height: 100dvh; /* 모바일 높이 대응 */
   position: relative;
+  background: transparent; /* 배경 가리지 않도록 */
 `;
 
-// 배경 이미지 (고정)
+/* 배경 이미지 (컨텍스트 값 반영) */
 S.Background = styled.div`
-  position: absolute;
+  position: fixed;        /* 스크롤과 무관하게 고정 */
   inset: 0;
-  background-image: url('/assets/images/background/snow.jpg');
+  background-image: url(${p => p.$bg || '/assets/images/background/snow.jpg'}); /* ← 변경 포인트 */
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
-  z-index: 0;
+  z-index: -1;            /* 내용/헤더 뒤로 보내기 */
 `;
 
-// 고정 헤더
+/* 고정 헤더 (컴포넌트 Header를 쓰면 이건 안 써도 됨) */
 S.Header = styled.header`
   position: absolute;
   top: 0;
@@ -73,17 +73,15 @@ S.Login = styled.div`
   }
 `;
 
-// 로그인폼
+/* 로그인/회원가입 영역 */
 S.ContentWrapper = styled.div`
   position: relative;
-  z-index: 1;
+  z-index: 1;            /* 배경 위에 오도록 */
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
+  min-height: 100dvh;
 `;
-
-
 
 export default S;
